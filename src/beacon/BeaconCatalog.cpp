1cb052cb87cd345dc38bf42cb6e90fe18c1b4ae6
Jason-skd <wintor76111@gmail.com>
2026-07-16 23:05:22 +0800

    feat(beacon): add points

--- src/beacon/BeaconCatalog.cpp
+++ src/beacon/BeaconCatalog.cpp
@@ -3,10 +3,13 @@
 namespace
 {
     const BeaconDefinition KnownBeacons[] = {
-        {{5, 6}, 1, "default beacon"},
         {{1, 1}, 1, "point 1"},
         {{1, 2}, 2, "point 2"},
         {{1, 3}, 3, "point 3"},
+        {{1, 4}, 4, "point 4"},
+        {{1, 5}, 5, "point 5"},
+        {{1, 6}, 6, "point 6"},
+        {{1, 7}, 7, "point 7"},
     };
 
     constexpr size_t KnownBeaconCount = sizeof(KnownBeacons) / sizeof(KnownBeacons[0]);
